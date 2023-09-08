ed599a66e46b3f08e8faa272d5ebd77ed2436736
brahimin <brahimhmidani09@gmail.com>
2023-09-07 19:00:31 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
-*main - main entry point
-*reurn: always 0(success)
-*/
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);/* Indicates successful execution */
+	return (0);
 }
