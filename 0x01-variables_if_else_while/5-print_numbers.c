521597cbe02f66e22862894e1c496da83fb6b5eb
brahimin <brahimhmidani09@gmail.com>
2023-09-08 17:14:01 -0700

    I hope that the earthquake does not cause any casualties

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,7 +4,7 @@
 /**
  * main - prints all single digit numbers of base 10 starting from 0,
  * followed by a new line.
- * return 0 always (success)
+ * Return: 0 Always (success)
  */
 int main(void)
 {
